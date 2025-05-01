542c826457aa1e2cd095803b65958d6049762d2f
chatteli-sami <samichatteli@gmail.com>
2025-05-01 17:43:38 +0100

    First Java commit

---
+++ Java Fundamentals/First Java Program/JavaProgram.java
@@ -0,0 +1,7 @@
+public class JavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Chatteli Sami");
+        System.out.println("I am 21 years old");
+        System.out.println("My hometwon is Tun");
+    }
+}
