ec244e0562aaa2e934c469c3321072c32c6b2fe5
chatteli-sami <samichatteli@gmail.com>
2025-05-29 14:37:45 +0100

    Hello World First Spring

---
+++ Java Spring/SpringProjects/helloworld/src/main/java/com/codingdojo/HomeController.java
@@ -0,0 +1,11 @@
+package com.codingdojo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/hello")
+public class HomeController {
+    
+}
